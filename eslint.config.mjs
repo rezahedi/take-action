2d6333b0d46bbb8ac600286a63cca30373f0e87d
Reza Zahedi <r.zahedi@gmail.com>
2025-06-09 17:22:21 -0700

    setup base configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginPrettier from 'eslint-plugin-prettier'
+import configPrettier from 'eslint-config-prettier'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+  }),
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
+    plugins: { prettier: pluginPrettier },
+    rules: {
+      ...configPrettier.rules, // Disable conflicting ESLint rules
+      'prettier/prettier': 'error', // Treat Prettier formatting issues as ESLint errors
+    },
+  },
 ];
 
 export default eslintConfig;
